77fd357cdd2cffc39c86276730520aae57d38de2
Madhav <varmamadhva71@gmail.com>
2024-04-09 11:47:56 +0530

    second commit\

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-function inputvalue(inputElementValue){
-    var inputvalues=inputElementValue;
-   console.log(inputvalues);
-}   
+var display = document.getElementById('display');
+
+function inputvalue(inputElementValue) {
+    var inputValue = inputElementValue;
+    if (display.value === 'Error') {
+        display.value = '';
+    }
+    if (inputValue === '+' || inputValue === '-' || inputValue === '*' || inputValue === '/') {
+        if (display.value.slice(-1) === '+' || display.value.slice(-1) === '-' || display.value.slice(-1) === '*' || display.value.slice(-1) === '/') {
+            display.value = display.value.slice(0, -1) + inputValue;
+        } else {
+            display.value += inputValue;
+        }
+    } else {
+        display.value += inputValue;
+    }
+}
+
+function deletelastentered() {
+    display.value = display.value.slice(0, -1);
+}
+
+function resetvalue() {
+    display.value = '';
+    display.placeholder = '0';
+}
+
+function result() {
+    try {
+        var result = eval(display.value);
+        display.value = result;
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
